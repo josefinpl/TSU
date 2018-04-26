abe4515ef346e8af7d428c1c2c631aa139418805
josefin palm <josefin-palm@hotmail.com>
2018-04-26 15:12:17 +0200

    små pill

--- Hitta/Hitta/Hitta/App.xaml.cs
+++ Hitta/Hitta/Hitta/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Hitta.Resources;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,8 +14,8 @@ namespace Hitta
 		{
 			InitializeComponent();
 
-			MainPage = new Hitta.MainPage();
-		}
+            MainPage = new Hitta.MainPage();
+        }
 
 		protected override void OnStart ()
 		{
